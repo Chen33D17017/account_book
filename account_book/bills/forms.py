21c7daa0a4c19c5a93ca2b32d8c7a00a4f87b7c1
Chen33D17017 <s894117@gmail.com>
2020-02-22 00:29:30 +0900

    change currency type

--- account_book/bills/forms.py
+++ account_book/bills/forms.py
@@ -5,7 +5,7 @@ from wtforms.fields.html5 import DateField
 from account_book.model import Category
 
 class BillInputForm(FlaskForm):
-    cost = IntegerField('Cost', validators=[DataRequired()], render_kw={"placeholder": "$"})
+    cost = IntegerField('Cost', validators=[DataRequired()], render_kw={"placeholder": "&#165;"})
     category = SelectField('Category')
     comment = StringField('Comment', validators=[DataRequired()], render_kw={"placeholder": "Dinner: KFC"})
     tax_rate = FloatField('Tax rate',default=0.08, render_kw={"placeholder": "Tax Rate"})
